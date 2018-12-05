f01500c0959e11bf503d68d7c8650b2640d637db
pbgaming <44928010+pbgaming@users.noreply.github.com>
2018-12-05 23:49:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -250,7 +250,7 @@ client.on('message', message => {
     }
 });
 
-             client.on("message", message => {
+client.on("message", message => {
     if (message.content === (prefix + "help")) {
           if(!message.guild.member(message.author).hasRole("help")) return undefined
      const embed = new Discord.RichEmbed() 
@@ -270,7 +270,7 @@ $show
    message.author.sendEmbed(embed)
    
    }
-   });  
+   });
 
 client.on('ready', () => {
    console.log(`----------------`);
